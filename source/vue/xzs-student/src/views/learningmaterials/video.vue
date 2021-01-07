593ef5813a01c6ae6bec7e8012b20d1b69796760
Gabriel <c16692162365@163.com>
2021-01-07 18:13:02 +0800

    更新卡片距离  -----csg

--- source/vue/xzs-student/src/views/learningmaterials/video.vue
+++ source/vue/xzs-student/src/views/learningmaterials/video.vue
@@ -5,10 +5,11 @@
         <el-menu-item index="1">Java视频</el-menu-item>
         <el-menu-item index="2">JS大全</el-menu-item>
         <el-menu-item index="3" >消息队列</el-menu-item>
-        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+        <el-menu-item index="4"><a href="https://www.bilibili.com/" target="_blank">bilibili</a></el-menu-item>
       </el-menu>
-        <el-col :span="3" v-for="(o, index) in 4" :key="o" :offset="index > 0 ? 2 : 0">
-          <el-card :body-style="{ left: '20px',top:'-20px' }">
+      <div class="col_card" style="margin-left:180px">
+        <el-col :span="4" v-for="(o, index) in 4" :key="o" :offset="index > 0 ? 2 : 0" style="margin: 25px">
+          <el-card :body-style="{ padding :'14px' }">
             <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
             <div style="padding: 14px;">
               <span>好吃的汉堡</span>
@@ -19,6 +20,7 @@
             </div>
           </el-card>
         </el-col>
+      </div>
       </el-row>
 </template>
 <script>
@@ -48,5 +50,8 @@ export default {
 <style scoped>
   .el-menu--horizontal>.el-menu-item {
     left: 180px;
+  }
+  .el-card__body{
+    margin-left: 150px;
   }
 </style>
